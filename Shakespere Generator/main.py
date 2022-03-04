09fe2ea5682088d30de9500b2c54bff7b67f33be
Radoslaw Krol <radoslaw.krol@icloud.com>
2022-03-04 19:49:38 +0100

    Commented

--- Shakespere Generator/main.py
+++ Shakespere Generator/main.py
@@ -7,19 +7,14 @@ from nltk.corpus import gutenberg
 
 warnings.filterwarnings('ignore')
 
+# uncomment below for initial download
 # nltk.download('gutenberg')
 
-
-# print(gutenberg.fileids())
 
 hamlet = gutenberg.raw('shakespeare-hamlet.txt')
 macbeth = gutenberg.raw('shakespeare-macbeth.txt')
 caesar = gutenberg.raw('shakespeare-caesar.txt')
 
-
-# print('\nRaw:\n', hamlet[:100])
-# print('\nRaw:\n', macbeth[:100])
-# print('\nRaw:\n', caesar[:100])
 
 def text_cleaner(text):
     text = re.sub(r'--', ' ', text)
@@ -47,12 +42,10 @@ macbeth_sents = ' '.join([sent.text for sent in macbeth_doc.sents if len(sent.te
 caesar_sents = ' '.join([sent.text for sent in caesar_doc.sents if len(sent.text) > 1])
 shakespeare_sents = hamlet_sents + macbeth_sents + caesar_sents
 
-# print(shakespeare_sents)
-
 generator_1 = markovify.Text(shakespeare_sents, state_size=3)
 
 
-# next we will use spacy's part of speech to generate more legible text
+# Use Spacy to generate more legible sentences
 class POSifiedText(markovify.Text):
     def word_split(self, sentence):
         return ['::'.join((word.orth_, word.pos_)) for word in nlp(sentence)]
@@ -62,12 +55,10 @@ class POSifiedText(markovify.Text):
         return sentence
 
 
-# Call the class on our text
 generator_2 = POSifiedText(shakespeare_sents, state_size=3)
 
-# now we will use the above generator to generate sentences
 for i in range(5):
     print(generator_2.make_sentence())
-# print 100 characters or less sentences
+
 for i in range(5):
     print(generator_2.make_short_sentence(max_chars=100))
